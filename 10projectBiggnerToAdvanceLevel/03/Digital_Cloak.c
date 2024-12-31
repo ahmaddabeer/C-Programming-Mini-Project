c37cea026d99ef688b1a1478c70d9649c76b73be
Your Name <ahmaddabeer36@gmail.com>
2024-12-31 20:31:11 +0530

    learn new concept

--- 10projectBiggnerToAdvanceLevel/03/Digital_Cloak.c
+++ 10projectBiggnerToAdvanceLevel/03/Digital_Cloak.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <time.h>
 
+void fill_time(char *);
+
 int main()
+{
+
+    char time[100];
+    fill_time(time);
+    int format;
+    printf("\n Choose the time format:");
+    printf("\n 1. 24 Hour format:");
+    printf("\n 1. 12 Hour format (default):");
+    printf("\n Make a choice(1/2):");
+    scanf("%d", &format);
+    printf("Current time :%s", time);
+
+    return 0;
+}
+
+void fill_time(char *buffer)
 {
 
     time_t raw_time;
 
     struct tm *current_time;
-    char buffer[50];
 
     time(&raw_time);
     current_time = localtime(&raw_time);
 
-    strftime(buffer, sizeof(buffer), "%H:%M:%S ", current_time);
-    
-    printf("Current time :%s", buffer);
-
-    return 0;
+    strftime(buffer, 100, "%I:%M:%S %p", current_time);
 }
