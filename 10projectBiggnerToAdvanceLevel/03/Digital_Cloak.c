421dbdaafbea20e8012d3544eb7cf04d0644a71e
Your Name <ahmaddabeer36@gmail.com>
2025-01-12 20:28:57 +0530

    add new line of code in this program

--- 10projectBiggnerToAdvanceLevel/03/Digital_Cloak.c
+++ 10projectBiggnerToAdvanceLevel/03/Digital_Cloak.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <time.h>
 
-void fill_time(char *);
+void fill_time(char *, int);
+void fill_date(char *);
 
+int input_format();
 int main()
 {
 
-    char time[100];
-    fill_time(time);
-    int format;
-    printf("\n Choose the time format:");
-    printf("\n 1. 24 Hour format:");
-    printf("\n 1. 12 Hour format (default):");
-    printf("\n Make a choice(1/2):");
-    scanf("%d", &format);
-    printf("Current time :%s", time);
+    char time[50], date[100];
+    int format = input_format();
+    fill_time(time, format);
+    fill_date(date);
+    printf("\nCurrent time :%s", time);
+    printf("\n Date :%s", Date);
 
     return 0;
 }
+int input_format()
+{
+    int format;
+    printf("\n Choose the time format:");
+    printf("\n 1. 24 Hour format:");
+    printf("\n 2. 12 Hour format (default):");
+    printf("\n Make a choice(1/2):");
+    scanf("%d", &format);
+    return format;
+}
+void fill_date(char* buffer)
 
-void fill_time(char *buffer)
+void fill_time(char *buffer, int format)
 {
 
     time_t raw_time;
@@ -28,6 +38,14 @@ void fill_time(char *buffer)
 
     time(&raw_time);
     current_time = localtime(&raw_time);
+    if (format == 1)
+    {
 
-    strftime(buffer, 100, "%I:%M:%S %p", current_time);
+        strftime(buffer, 100, "%H:%M:%S %p", current_time);
+    }
+    else
+    {
+
+        strftime(buffer, 100, "%I:%M:%S %p", current_time);
+    }
 }
